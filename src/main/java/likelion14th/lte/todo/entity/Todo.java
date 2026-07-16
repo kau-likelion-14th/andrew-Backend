c9992ed8fde31ffebe5d8f3bf619e7fa7e70e682
TomorrowIppo <bjk1624@gmail.com>
2026-07-16 19:41:34 +0900

    feat: 제공받은 파일 추가

---
+++ src/main/java/likelion14th/lte/todo/entity/Todo.java
@@ -0,0 +1,46 @@
+package likelion14th.lte.todo.entity;
+
+import jakarta.persistence.*;
+import likelion14th.lte.Entity.BaseEntity;
+import likelion14th.lte.category.entity.Category;
+import likelion14th.lte.user.entity.User;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "todo")
+public class Todo extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean routineEnabled;
+
+    @Column
+    private LocalDate startDate;
+
+    @Column
+    private LocalDate endDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    private WeekEnum week;
+}
